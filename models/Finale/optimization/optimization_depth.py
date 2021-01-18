f78df2709599251d1bbff3e85b112108af8294b5
XezXey <puntawat-1@hotmail.com>
2021-01-18 13:07:52 +0700

    Error analysis with angle, fixing the axix and scale

---
+++ models/Finale/optimization/optimization_depth.py
@@ -0,0 +1,124 @@
+import torch as pt
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+# TrajectoryOptimization Class
+class TrajectoryOptimizationDepth(pt.nn.Module):
+  def __init__(self, uv, cam_params_dict, input_dict, latent_size, model_dict, pred_dict, latent_code):
+    super(TrajectoryOptimizationDepth, self).__init__()
+    self.uv = uv
+    self.cam_params_dict = cam_params_dict
+    self.input_dict = input_dict
+    self.model_dict = model_dict
+    self.pred_dict = pred_dict
+    self.latent_code = latent_code
+    if 'angle' in self.latent_code:
+      latent_size -= 1
+    if 'f' in self.latent_code and 'fnorm' in self.latent_code:
+      latent_size -= 3
+    self.latent_size = latent_size
+    self.latent = pt.nn.ParameterList()
+    self.construct_latent()
+
+  def forward(self):
+    latent = self.update_latent()
+    latent = self.manipulate_latent(latent)
+    in_f = pt.cat((self.uv, latent), dim=2)
+    pred_depth, (_, _) = self.model_dict['model_depth'](in_f=in_f, lengths=self.input_dict['lengths'])
+    return pred_depth
+
+  def construct_latent(self):
+    # This use EOT to split the latent. Stack and repeat to have the same shape with the "uv" 
+    flag = self.pred_dict['model_flag']
+    lengths = self.input_dict['lengths']
+    close = pt.isclose(flag, pt.tensor(1.), atol=5e-1)
+    latent_list = []
+    for i in range(flag.shape[0]):
+      where = pt.where(close[i] == True)[0]
+      where = where[where < lengths[i]]
+      if len(where) == 0:
+        # Flag prediction goes wrong
+        # latent = pt.nn.Parameter(pt.rand(1, self.latent_size, dtype=pt.float32).cuda(), requires_grad=True).cuda()
+        self.latent.append(pt.nn.Parameter(pt.rand(1, self.latent_size, dtype=pt.float32).cuda() * 100., requires_grad=True).cuda())
+      else:
+        # Flag prediction work properly
+        n_latent = len(where)
+        # latent = pt.nn.Parameter(pt.rand(n_latent+1, self.latent_size, dtype=pt.float32).cuda(), requires_grad=True).cuda()
+        self.latent.append(pt.nn.Parameter(pt.rand(n_latent+1, self.latent_size, dtype=pt.float32).cuda() * 100., requires_grad=True).cuda())
+
+    return latent_list
+
+  def update_latent(self):
+    '''
+    Return : latent with shape = (batchsize, seq_len, latent_size)
+    '''
+    flag = self.pred_dict['model_flag']
+    lengths = self.input_dict['lengths']
+    close = pt.isclose(flag, pt.tensor(1.), atol=5e-1)
+    all_latent = pt.ones((flag.shape[0], flag.shape[1], self.latent_size)).cuda()
+    for i in range(flag.shape[0]):
+      # Each Trajectory
+      where = pt.where(close[i] == True)[0]
+      where = where[where < lengths[i]]
+      if len(where) == 0:
+        where = [0] + [flag.shape[1]]
+      else:
+        where = [0] + list(where.cpu().detach().numpy()+1) + [flag.shape[1]]
+      for j in range(len(where)-1):
+        all_latent[i][where[j]:where[j+1]] = self.latent[i][j].repeat(where[j+1] - where[j], 1)
+
+      # all_latent.append(pt.cat(each_latent))
+      # plt.plot(all_latent[i].cpu().detach().numpy(), 'r-o')
+      # plt.plot(flag[i].cpu().detach().numpy(), 'g-o')
+      # plt.plot(close[i].cpu().detach().numpy(), 'y-o')
+      # plt.show()
+
+    return all_latent
+
+  def manipulate_latent(self, latent):
+    remaining_size = self.latent_size
+    latent_pointer = 0
+    latent_in = []
+    if 'angle' in self.latent_code and remaining_size > 0:
+      #####################################
+      ############### ANGLE ###############
+      #####################################
+      latent_angle = pt.cat((pt.sin(latent[..., [latent_pointer]]), pt.cos(latent[..., [latent_pointer]])), dim=2)
+      latent_in.append(latent_angle)
+      print("Manipulate Angle : ", latent_angle.shape)
+      remaining_size -= 1
+      latent_pointer += 1
+    if 'f' in self.latent_code and 'f_norm' not in self.latent_code and remaining_size > 0:
+      #####################################
+      ############### FORCE ###############
+      #####################################
+      latent_f = latent[..., latent_pointer:latent_pointer+3]
+      latent_in.append(latent_f)
+      print("Manipulate Force : ", latent_f.shape)
+      remaining_size -= 1
+      latent_pointer += 3
+    if 'f' not in self.latent_code and 'f_norm' in self.latent_code and remaining_size > 0:
+      #####################################
+      ############# FORCE Norm ############
+      #####################################
+      latent_fnorm = latent[..., latent_pointer:latent_pointer+3] / pt.sqrt(pt.sum(latent[..., latent_pointer:latent_pointer+3]**2, dim=1, keepdims=True) + 1e-16)
+      latent_in.append(latent_fnorm)
+      print("Manipulate Force Norm : ", latent_fnorm.shape)
+      remaining_size -= 3
+      latent_pointer += 3
+    if 'f' in self.latent_code and 'f_norm' in self.latent_code and remaining_size > 0:
+      #####################################
+      ######### FORCE & FORCE Norm ########
+      #####################################
+      latent_f = latent[..., latent_pointer:latent_pointer+3]
+      latent_fnorm = latent_f / pt.sqrt(pt.sum(latent_f**2, dim=1, keepdims=True) + 1e-16)
+      latent_in.append(latent_f)
+      latent_in.append(latent_fnorm)
+      print("Manipulate Force : ", latent_f.shape)
+      print("Manipulate Force Norm : ", latent_fnorm.shape)
+      remaining_size -= 3
+      latent_pointer += 3
+
+    latent_in = pt.cat(latent_in, dim=2)
+    return latent_in
